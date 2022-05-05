3ae1e9b880d98d45455adffc92380160db16f6b7
chagu <chaguer4@gmail.com>
2022-05-05 00:50:24 +0000

    [hicham];[listenner page added];[nowarning]

---
+++ app/src/main/java/com/hichamch/bookivia/listener.java
@@ -0,0 +1,44 @@
+package com.hichamch.bookivia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class listener extends AppCompatActivity {
+Button btn_play2_listener,btn_play_listener;
+  MediaPlayer player;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listener);
+        btn_play2_listener = findViewById(R.id.btn_play2_listener);
+        btn_play_listener = findViewById(R.id.btn_play_listener);
+
+    }
+    public void play(View v){
+        if(player == null)
+        {
+            btn_play_listener.setBackgroundResource(R.drawable.pauseicon2);
+            btn_play2_listener.setBackgroundResource(R.drawable.ic_pause);
+            player = MediaPlayer.create(this,R.raw.loseyouself);
+            player.start();
+        }
+
+        else
+        {
+            btn_play_listener.setBackgroundResource(R.drawable.playicon);
+            btn_play2_listener.setBackgroundResource(R.drawable.ic_play);
+           player.pause();
+        }
+    }
+
+
+}
